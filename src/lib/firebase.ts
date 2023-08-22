a6ca48dead4a3f05ca2505381d129f7a3a1d95d1
Ericsen Tsai <bommer95175@gmail.com>
2023-08-23 00:22:45 +0800

    refactor: cast env type

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,9 +3,9 @@ import { initFirestore } from '@auth/firebase-adapter'
 
 const firestore = initFirestore({
   credential: cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n') || '',
+    projectId: process.env.FIREBASE_PROJECT_ID as string,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL as string,
+    privateKey: (process.env.FIREBASE_PRIVATE_KEY as string).replace(/\\n/g, '\n'),
   }),
 })
 
